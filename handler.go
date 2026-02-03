fda7df0e5b11ca2109aa2fc30ba326ae3eb48d60
Airlangga <airlanggakrishna@gmail.com>
2026-02-03 23:37:45 +0700

    feat: create user

--- handler.go
+++ handler.go
@@ -1,6 +1,8 @@
 package main
 
-import "database/sql"
+import (
+	"database/sql"
+)
 
 type Handler struct {
 	DB *sql.DB
@@ -11,7 +13,22 @@ func NewHandler(db *sql.DB) *Handler {
 }
 
 // create user
-
+func (h *Handler) CreateUser(email, password string) error {
+	_, err := h.DB.Exec(
+		`INSERT INTO users
+			(email, password)
+		VALUES
+			(?, ?)
+		`,
+		email, password,
+	)
+	
+	if err != nil {
+		return err
+	}
+	
+	return nil
+}
 
 // read user by email
 
