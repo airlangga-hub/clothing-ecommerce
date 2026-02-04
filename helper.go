fa226d23dfdf2f0dcd8290ad4a8545cd7d3d563f
Airlangga <airlanggakrishna@gmail.com>
2026-02-04 07:09:58 +0700

    format: some formatting

--- helper.go
+++ helper.go
@@ -10,11 +10,11 @@ func PrintStdOut(buf *bytes.Buffer) {
 	lines := strings.Split(strings.TrimSpace(buf.String()), "\n")
 	tableWidth := len(lines[0])
 	separator := "+" + strings.Repeat("-", tableWidth-2) + "+"
-	
+
 	fmt.Println(separator)
 	fmt.Println(lines[0])
 	fmt.Println(separator)
-	
+
 	for _, line := range lines[1:] {
 		fmt.Println(line)
 	}
